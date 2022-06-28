f81c579cdffaf3aa1621a13a314368171a0282a0
Geisty <geistybear@gmail.com>
2022-06-28 18:19:28 -0400

    controls js

--- controls.js
+++ controls.js
@@ -21,6 +21,25 @@ class Controls{
                     this.reverse=true;
                     break;
             }
+            console.table(this);
+        }
+        this.onkeyup=(event)=>{
+            document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+            console.table(this);
         }
     }
 }
