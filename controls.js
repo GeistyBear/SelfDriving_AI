08c7661a77913d91bfb2594fd05b632277d35d6a
Geisty <geistybear@gmail.com>
2022-06-28 18:59:06 -0400

    more fun!

--- controls.js
+++ controls.js
@@ -1,45 +1,44 @@
-class Controls{
-    constructor(){
-        this.forward=false;
-        this.left=false;
-        this.right=false;
-        this.reverse=false;
-        }
-        this.#addKeyboardListeners(){
-            document.onkeydown=(event)=>{
-            switch(event.key){
-                case "ArrowLeft":
-                    this.left=true;
-                    break;
-                case "ArrowRight":
-                    this.right=true;
-                    break;
-                case "ArrowUp":
-                    this.forward=true;
-                    break;
-                case "ArrowDown":
-                    this.reverse=true;
-                    break;
-            }
-            console.table(this);
-        }
-        this.onkeyup=(event)=>{
-            document.onkeydown=(event)=>{
-            switch(event.key){
-                case "ArrowLeft":
-                    this.left=true;
-                    break;
-                case "ArrowRight":
-                    this.right=true;
-                    break;
-                case "ArrowUp":
-                    this.forward=true;
-                    break;
-                case "ArrowDown":
-                    this.reverse=true;
-                    break;
-            }
-            console.table(this);
-        }
-    }
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+  }
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.left = true;
+          break;
+        case 'ArrowRight':
+          this.right = true;
+          break;
+        case 'ArrowUp':
+          this.forward = true;
+          break;
+        case 'ArrowDown':
+          this.reverse = true;
+          break;
+      }
+      console.table(this);
+    };
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.left = false;
+          break;
+        case 'ArrowRight':
+          this.right = false;
+          break;
+        case 'ArrowUp':
+          this.forward = false;
+          break;
+        case 'ArrowDown':
+          this.reverse = false;
+          break;
+      }
+      console.table(this);
+    };
+  }
 }
