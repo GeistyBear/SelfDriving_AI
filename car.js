3080f6dff770b458a2945d3a60e376b640fb7e46
Geisty <geistybear@gmail.com>
2022-06-29 06:05:33 -0400

    changes

--- car.js
+++ car.js
@@ -20,7 +20,7 @@ class Car {
       this.speed -= 2;
     }
     if (this.speed < -this.maxSpeed / 2) {
-      this.speed -= this.maxSpeed / 2;
+      this.speed = -this.maxSpeed / 2;
     }
     if (this.speed > 0) {
       this.speed -= this.friction;
