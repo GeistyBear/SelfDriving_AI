3c8e76019b3dea5b30921033601f44cf98dd29d0
Geisty <geistybear@gmail.com>
2022-06-29 05:27:29 -0400

    main.js added animate function

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const canvas = document.getElementById('myCanvas');
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
+
+Animate();
+
+function animate() {
+  car.update();
+  car.draw(ctx);
+  canvas.height = window.innerHeight;
+  requestAnimationFrame(animate);
+}
