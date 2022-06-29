79ef4f64ea5e7355853c2b70e0daa3f2ed4cdb50
Geisty <geistybear@gmail.com>
2022-06-29 06:02:42 -0400

    car class speed changes

--- car.js
+++ car.js
@@ -4,18 +4,32 @@ class Car {
     this.y = y;
     this.width = width;
     this.height = height;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
 
     this.speed = 0;
     this.acceleration = 0.2;
+
     this.controls = new Controls();
   }
   update() {
     if (this.controls.forward) {
-      this.y += this.acceleration;
+      this.speed += this.acceleration;
     }
     if (this.controls.reverse) {
-      this.y -= 2;
+      this.speed -= 2;
     }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed -= this.maxSpeed / 2;
+    }
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    this.y -= this.speed;
   }
 
   draw(ctx) {
