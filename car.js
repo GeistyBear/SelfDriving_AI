21ea4595f8e7b8b15865b74ced954589cf694570
Geisty <geistybear@gmail.com>
2022-06-29 10:35:37 -0400

    car js doc

--- car.js
+++ car.js
@@ -34,12 +34,14 @@ class Car {
     if (Math.abs(this.speed) < this.friction) {
       this.speed = 0;
     }
-
-    if (this.controls.left) {
-      this.angle += 0.03;
-    }
-    if (this.controls.right) {
-      this.angle -= 0.03;
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
     this.x -= Math.sin(this.angle) * this.speed;
     this.x -= Math.cos(this.angle) * this.speed;
