6952bf023d631f5e3184f3676a77ee813f19be81
Geisty <geistybear@gmail.com>
2022-06-29 06:54:04 -0400

    fixed main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
 
 animate();
 
 function animate() {
   car.update();
-  car.draw(ctx);
+
   canvas.height = window.innerHeight;
+  car.draw(ctx);
   requestAnimationFrame(animate);
 }
